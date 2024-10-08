e703e9b03af2afd92f6390c816b7b0f733bf1638
dimbo1324 <dim4dmi7rij@yandex.ru>
2024-10-08 16:42:05 +0300

    https://reactflow.dev/examples/edges/markers

--- FrontEnd/src/MiniMap/System.jsx
+++ FrontEnd/src/MiniMap/System.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import {ReactFlow, MiniMap} from '@xyflow/react';
+import {ReactFlow} from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import {defaultNodes} from "./nodes.jsx";
 import {defaultEdges} from "./edges.jsx";
-
-
-const nodeColor = (node) => {
-	switch (node.type) {
-		case 'input':
-			return '#6ede87';
-		case 'output':
-			return '#6865A5';
-		default:
-			return '#ff0072';
-	}
-};
 
 export default function System() {
 	return (
 		<div style={{width: '100%', height: '100vh'}}> {/* Полная высота для графа */}
 			<ReactFlow defaultNodes={defaultNodes} defaultEdges={defaultEdges} fitView>
-				<MiniMap nodeColor={nodeColor} nodeStrokeWidth={3} zoomable pannable/>
 			</ReactFlow>
 		</div>
 	);
--- FrontEnd/src/MiniMap/nodes.jsx
+++ FrontEnd/src/MiniMap/nodes.jsx
@@ -7,19 +7,16 @@ export const defaultNodes = [
 		type: 'input',
 		data: {label: 'Input Node'},
 		position: {x: 250, y: 25},
-		style: {backgroundColor: '#6ede87', color: 'white'},
 	},
 	{
 		id: '2',
-		data: {label: <div>Default Node</div>},
+		data: {label: <div> Default Node</div>},
 		position: {x: 100, y: 125},
-		style: {backgroundColor: '#ff0072', color: 'white'},
 	},
 	{
 		id: '3',
 		type: 'output',
 		data: {label: 'Output Node'},
 		position: {x: 250, y: 250},
-		style: {backgroundColor: '#6865A5', color: 'white'},
 	},
 ];
