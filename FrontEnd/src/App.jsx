70e3e77dbb2c5d8e05152932f9e1710324a1db8c
dimbo1324 <dim4dmi7rij@yandex.ru>
2024-10-08 16:19:42 +0300

    ....

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,36 +1,12 @@
-import {useState} from 'react';
-import ReactFlow, {addEdge, Background, Controls} from 'reactflow';
-import 'reactflow/dist/style.css';  // Добавляем стили для React Flow
+import React from 'react';
+import './App.css';
+import '@xyflow/react/dist/style.css';
+import System from "./MiniMap/System.jsx";
 
 export default function App() {
-	// Определяем начальные узлы и рёбра (edges)
-	const initialNodes = [
-		{id: '1', position: {x: 250, y: 5}, data: {label: 'Start Node'}},
-		{id: '2', position: {x: 100, y: 100}, data: {label: 'Another Node'}},
-	];
-	const initialEdges = [{id: 'e1-2', source: '1', target: '2', type: 'default'}];
-	
-	const [nodes, setNodes] = useState(initialNodes);
-	const [edges, setEdges] = useState(initialEdges);
-	
-	// Функции для изменения узлов и рёбер
-	const onNodesChange = (changes) => setNodes((nds) => nds.map((n) => ({...n, ...changes})));
-	const onEdgesChange = (changes) => setEdges((eds) => eds.map((e) => ({...e, ...changes})));
-	const onConnect = (params) => setEdges((eds) => addEdge(params, eds));
-	
 	return (
-		<div style={{height: '100vh'}}>
-			<ReactFlow
-				nodes={nodes}
-				edges={edges}
-				onNodesChange={onNodesChange}
-				onEdgesChange={onEdgesChange}
-				onConnect={onConnect}
-				fitView
-			>
-				<Controls/> {/* Панель управления для масштабирования */}
-				<Background/> {/* Фон с сеткой */}
-			</ReactFlow>
+		<div style={{width: '100%', height: '100vh'}}> {/* Полная высота для графа */}
+			<System></System>
 		</div>
 	);
 }
---
+++ FrontEnd/src/MiniMap/nodes.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '@xyflow/react/dist/style.css';
+
+export const defaultNodes = [
+	{
+		id: '1',
+		type: 'input',
+		data: {label: 'Input Node'},
+		position: {x: 250, y: 25},
+		style: {backgroundColor: '#6ede87', color: 'white'},
+	},
+	{
+		id: '2',
+		data: {label: <div>Default Node</div>},
+		position: {x: 100, y: 125},
+		style: {backgroundColor: '#ff0072', color: 'white'},
+	},
+	{
+		id: '3',
+		type: 'output',
+		data: {label: 'Output Node'},
+		position: {x: 250, y: 250},
+		style: {backgroundColor: '#6865A5', color: 'white'},
+	},
+];
