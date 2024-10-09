c41130c3750f5e06d443c5903f9906f9a08c4bfc
dimbo1324 <dim4dmi7rij@yandex.ru>
2024-10-09 08:57:09 +0300

    ....

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
-import '@xyflow/react/dist/style.css';
-import System from "./Graph_v_1/System.jsx";
 
 export default function App() {
 	return (
-		<div style={{width: '100%', height: '100vh'}}> {/* Полная высота для графа */}
-			<System></System>
-		</div>
+		<div></div>
 	);
 }
