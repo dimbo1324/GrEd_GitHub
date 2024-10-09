3384174752816d66455c998d06058b83ded20e74
dimbo1324 <dim4dmi7rij@yandex.ru>
2024-10-09 11:19:33 +0300

    ...

--- FrontEnd/src/Graphs_module_v1/elements.jsx
+++ FrontEnd/src/Graphs_module_v1/elements.jsx
@@ -10,6 +10,13 @@ export const elements = [
 	// Группа №2
 	{data: {id: 'GROUP_2'}, classes: 'compound'},
 	
+	
+	{data: {id: 'templates_node_5', parent: '', label: 'node_5'}},
+	{data: {id: 'templates_node_6', parent: '', label: 'node_6'}},
+	
+	
 	// Связи
 	{data: {source: 'index_js_GROUP_1', target: 'templates_js'}},
+	{data: {source: 'templates_node_5', target: 'templates_node_6'}},
+	{data: {source: 'templates_node_6', target: 'templates_node_5'}},
 ];
--- FrontEnd/src/Graphs_module_v1/styles.jsx
+++ FrontEnd/src/Graphs_module_v1/styles.jsx
@@ -1,36 +1,37 @@
 export const stylesheet = [
-  {
-    selector: 'node',
-    style: {
-      'background-color': '#d9ad60',
-      'label': 'data(label)',
-      'shape': 'rectangle',
-      'text-valign': 'center',
-      'text-halign': 'center',
-      'width': '100px',
-      'height': '60px',
-      'border-width': 2,
-      'border-color': '#333',
-    },
-  },
-  {
-    selector: '.compound',
-    style: {
-      'shape': 'round-rectangle',
-      'background-opacity': 0.1,
-      'border-width': 2,
-      'border-color': '#333',
-      'padding': 10,
-      'label': 'data(id)',
-    },
-  },
-  {
-    selector: 'edge',
-    style: {
-      'width': 2,
-      'line-color': '#2374ab',
-      'target-arrow-color': '#2374ab',
-      'target-arrow-shape': 'triangle',
-    },
-  },
+	{
+		selector: 'node',
+		style: {
+			'background-color': '#d9ad60',
+			'label': 'data(label)',
+			'shape': 'rectangle',
+			'text-valign': 'center',
+			'text-halign': 'center',
+			'width': '100px',
+			'height': '60px',
+			'border-width': 2,
+			'border-color': '#333',
+		},
+	},
+	{
+		selector: '.compound',
+		style: {
+			'shape': 'round-rectangle',
+			'background-opacity': 0.1,
+			'border-width': 2,
+			'border-color': '#333',
+			'padding': 10,
+			'label': 'data(id)',
+		}
+	},
+	{
+		selector: 'edge',
+		style: {
+			'width': 2,  // Толщина ребра
+			'line-color': '#2374ab',  // Цвет линии ребра
+			'target-arrow-color': '#2374ab',  // Цвет стрелки
+			'target-arrow-shape': 'triangle',  // Форма стрелки
+			'curve-style': 'bezier',  // Для плавных рёбер (если нужно)
+		}
+	}
 ];
